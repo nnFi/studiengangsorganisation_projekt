1532ef88415345d9672ee60fa864c8cf51419aee
Finn Plassmeier <finn@plassmeier.email>
2024-04-24 17:45:12 +0200

    RequestMapping

--- src/main/java/com/projekt/studiengangsorganisation/controller/TestController.java
+++ src/main/java/com/projekt/studiengangsorganisation/controller/TestController.java
@@ -9,16 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("test/")
-public String requestMethodName(@RequestParam String param) {
-    return new String();
-}
-
 public class TestController {
 
     @Autowired

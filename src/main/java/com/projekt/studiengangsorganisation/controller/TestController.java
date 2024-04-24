f5b7e223e4702c8e0674cf5fbd710108f0dc9e9a
Finn Plassmeier <finn@plassmeier.email>
2024-04-24 17:43:43 +0200

    Request einbau

--- src/main/java/com/projekt/studiengangsorganisation/controller/TestController.java
+++ src/main/java/com/projekt/studiengangsorganisation/controller/TestController.java
@@ -8,8 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
+@RequestMapping("test/")
+public String requestMethodName(@RequestParam String param) {
+    return new String();
+}
+
 public class TestController {
 
     @Autowired

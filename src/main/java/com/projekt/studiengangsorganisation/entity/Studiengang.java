d522b9aec98d58d53b0b84154e1fb08a4946f9bb
Paul Rakow <parakow@gmail.com>
2024-04-25 12:47:31 +0200

    Refactor Studiengang entity class and update relationships

--- src/main/java/com/projekt/studiengangsorganisation/entity/Studiengang.java
+++ src/main/java/com/projekt/studiengangsorganisation/entity/Studiengang.java
@@ -10,39 +10,34 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="studiengang")
+@Table(name = "studiengang")
 public class Studiengang {
 
-
     @Id
-    @Column(name="studiengang_id")
+    @Column(name = "studiengang_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name="studiengang_name")
+    @Column(name = "studiengang_name")
     private String name;
 
-    @Column(name="studiengang_abschluss")
+    @Column(name = "studiengang_abschluss")
     private String abschluss;
 
-    @Column(name="studiengang_regelstudienzeit")
+    @Column(name = "studiengang_regelstudienzeit")
     private int regelstudienzeit;
 
     @ManyToOne
-    @JoinColumn(name="leiter_id")
+    @JoinColumn(name = "leiter_id")
     private Mitarbeiter leiter;
 
     @ManyToOne
-    @JoinColumn(name="stellvertretenderLeiter_id")
+    @JoinColumn(name = "stellvertretenderLeiter_id")
     private Mitarbeiter stellvertretenderLeiter;
-    
-    @ManyToOne
-    @JoinColumn(name="fachbereich_id")
-    private Fachbereich fachbereich;
 
     @ManyToOne
-    @JoinColumn(name="pruefungsordnung_id")
-    private Pruefungsordnung pruefungsordung;
+    @JoinColumn(name = "fachbereich_id")
+    private Fachbereich fachbereich;
 
     public long getId() {
         return id;
@@ -99,15 +94,5 @@ public class Studiengang {
     public void setFachbereich(Fachbereich fachbereich) {
         this.fachbereich = fachbereich;
     }
-
-    public Pruefungsordnung getPruefungsordung() {
-        return pruefungsordung;
-    }
-
-    public void setPruefungsordung(Pruefungsordnung pruefungsordung) {
-        this.pruefungsordung = pruefungsordung;
-    }
-
-    
 
 }

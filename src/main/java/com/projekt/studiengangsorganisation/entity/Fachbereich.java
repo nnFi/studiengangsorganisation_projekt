84c16b6f5c986f6653fcb917af65e82c65fdea27
Finn Plassmeier <finn@plassmeier.email>
2024-04-25 12:18:08 +0200

    Fachbereich Kardinalitäten

--- src/main/java/com/projekt/studiengangsorganisation/entity/Fachbereich.java
+++ src/main/java/com/projekt/studiengangsorganisation/entity/Fachbereich.java
@@ -1,10 +1,16 @@
 package com.projekt.studiengangsorganisation.entity;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,9 +22,41 @@ public class Fachbereich{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Nutzer getReferent() {
+        return referent;
+    }
+
+    public void setReferent(Nutzer referent) {
+        this.referent = referent;
+    }
+
+    public Nutzer getStellvertreter() {
+        return stellvertreter;
+    }
+
+    public void setStellvertreter(Nutzer stellvertreter) {
+        this.stellvertreter = stellvertreter;
+    }
+
+    public Set<Fachgruppe> getFachgruppen() {
+        return fachgruppen;
+    }
+
+    public void setFachgruppen(Set<Fachgruppe> fachgruppen) {
+        this.fachgruppen = fachgruppen;
+    }
+
+    @ManyToOne()
     @Column(name = "fachbereich_referent")
+    @JoinColumn(name = "referent_id")
     private Nutzer referent;
 
+    @ManyToMany()
     @Column(name = "fachbereich_stellvertreter")
+    @JoinColumn(name = "stellvertreter_id")
     private Nutzer stellvertreter;
+
+    @ManyToMany
+    @JoinColumn(name = "fachgruppe_id")
+    Set<Fachgruppe> fachgruppen;
 }

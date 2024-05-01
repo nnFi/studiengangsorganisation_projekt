1182006c2a1389682a24067799270f5f7baedeb5
Bao <baochaukathi@gmx.de>
2024-05-01 15:49:15 +0200

    modulgruppe service

---
+++ src/main/java/com/projekt/studiengangsorganisation/service/ModulgruppeService.java
@@ -0,0 +1,28 @@
+package com.projekt.studiengangsorganisation.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projekt.studiengangsorganisation.entity.Modulgruppe;
+import com.projekt.studiengangsorganisation.repository.ModulgruppeRepository;
+
+@Service
+public class ModulgruppeService {
+    @Autowired
+    ModulgruppeRepository modulgruppeRepository;
+
+    public ModulgruppeService() {
+
+    }
+
+    public List<Modulgruppe> getModulgruppe() {
+        return modulgruppeRepository.findAll();
+    }
+
+    public Optional<Modulgruppe> getFachbereich(String id) {
+        return modulgruppeRepository.findById(Long.parseLong(id));
+    }
+}

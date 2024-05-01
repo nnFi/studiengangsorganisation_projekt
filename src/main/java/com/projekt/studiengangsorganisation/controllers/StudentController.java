29bc35f6de5b9b027f5ae57dcc0338b4fad30daf
Finn Plassmeier <finn@plassmeier.email>
2024-05-01 17:35:22 +0200

    controller

---
+++ src/main/java/com/projekt/studiengangsorganisation/controllers/StudentController.java
@@ -0,0 +1,43 @@
+package com.projekt.studiengangsorganisation.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.projekt.studiengangsorganisation.entity.Student;
+import com.projekt.studiengangsorganisation.service.StudentService;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RequestMapping("/student")
+@RestController
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/{id}")
+    public Student getOne(@RequestParam String id) {
+        Optional<Student> student = studentService.getStudent(id);
+
+        if (student.isPresent()) {
+            return student.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("")
+    public List<Student> getAll(HttpServletResponse response) {
+        List<Student> list = studentService.getStudenten();
+        response.setHeader("Content-Range", "1-" + list.size());
+        return list;
+    }
+}

73e0af9809053bdf3348ba41507cd8dd9f763d19
Erkan Yüzer <erkan.yuezer@gmail.com>
2024-05-01 16:03:56 +0200

    Mitarbeiter/Nutzer Services

---
+++ src/main/java/com/projekt/studiengangsorganisation/service/NutzerService.java
@@ -0,0 +1,33 @@
+package com.projekt.studiengangsorganisation.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projekt.studiengangsorganisation.entity.Nutzer;
+import com.projekt.studiengangsorganisation.repository.NutzerRepository;
+
+@Service
+public class NutzerService {
+    
+@Autowired
+NutzerRepository nutzerRepository;
+
+public NutzerService() {
+
+}
+
+public List<Nutzer> getNutzer() {
+return nutzerRepository.findAll();
+}
+
+
+public Optional<Nutzer> getNutzer(String id) {
+    return nutzerRepository.findById(Long.parseLong(id));
+}
+
+
+    
+}

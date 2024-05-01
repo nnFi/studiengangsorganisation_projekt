36b07d0f851abe9a12aba56139afed5a6466cce5
Bao <baochaukathi@gmx.de>
2024-05-01 16:06:43 +0200

    modulservice

--- src/main/java/com/projekt/studiengangsorganisation/service/ModulService.java
+++ src/main/java/com/projekt/studiengangsorganisation/service/ModulService.java
@@ -25,4 +25,22 @@ public class ModulService {
     public Optional<Modul> getModul(String id) {
         return modulRepository.findById(Long.parseLong(id));
     }
+
+    public void insertTestData() {
+        Modul modul = new Modul();
+        modul.setName("Programmieren");
+        modul.setId(1l);
+        modul.setKuerzel("5 WIF 22");
+        modul.setWorkload(180);
+        modul.setAbschluss("B. Sc.");
+        modul.setArt("Seminaristischer Unterricht");
+        modul.setBeschreibung("Programmieren lernen");
+        modul.setCredits(6);
+        modul.setDauer(1);
+        modul.setFreigegeben(true);
+        modul.setLehrveranstaltungsort("D2");
+
+        modulRepository.save(modul);
+
+    }
 }

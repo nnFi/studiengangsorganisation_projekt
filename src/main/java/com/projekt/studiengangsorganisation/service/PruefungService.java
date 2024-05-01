fa1153228a7501195f80cef977e3750223a35083
Finn Plassmeier <finn@plassmeier.email>
2024-05-01 15:55:16 +0200

    Services

---
+++ src/main/java/com/projekt/studiengangsorganisation/service/PruefungService.java
@@ -0,0 +1,22 @@
+package com.projekt.studiengangsorganisation.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projekt.studiengangsorganisation.entity.Pruefung;
+import com.projekt.studiengangsorganisation.repository.PruefungRepository;
+
+@Service
+public class PruefungService {
+
+    @Autowired
+    PruefungRepository pruefungRepository;
+
+    public PruefungService() {}
+
+    public List<Pruefung> getPruefungen() {
+        return pruefungRepository.findAll();
+    }
+}
---
+++ src/main/java/com/projekt/studiengangsorganisation/service/PruefungsordnungService.java
@@ -0,0 +1,26 @@
+package com.projekt.studiengangsorganisation.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projekt.studiengangsorganisation.entity.Pruefungsordnung;
+import com.projekt.studiengangsorganisation.repository.PruefungsordnungRepository;
+
+@Service
+public class PruefungsordnungService {
+    @Autowired
+    PruefungsordnungRepository pruefungsordnungRepository;
+
+    public PruefungsordnungService() {}
+
+    public List<Pruefungsordnung> getFachgruppen() {
+        return pruefungsordnungRepository.findAll();
+    }
+
+    public Optional<Pruefungsordnung> getFachgruppe(String id) {
+        return pruefungsordnungRepository.findById(Long.parseLong(id));
+    }
+}

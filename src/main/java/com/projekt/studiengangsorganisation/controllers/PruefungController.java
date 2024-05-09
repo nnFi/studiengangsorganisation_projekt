e2838b2f6564d8e7b24c50e97105af120b1979a3
Paul Rakow <parakow@gmail.com>
2024-05-09 12:43:42 +0200

    refactor: Add endpoint to PruefungController for retrieving a single Pruefung by ID

--- src/main/java/com/projekt/studiengangsorganisation/controllers/PruefungController.java
+++ src/main/java/com/projekt/studiengangsorganisation/controllers/PruefungController.java
@@ -1,11 +1,15 @@
 package com.projekt.studiengangsorganisation.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.projekt.studiengangsorganisation.entity.Pruefung;
 import com.projekt.studiengangsorganisation.service.PruefungService;
@@ -18,6 +22,17 @@ public class PruefungController {
 
     @Autowired
     PruefungService pruefungService;
+
+    @GetMapping("/{id}")
+    public Pruefung getOne(@PathVariable String id) {
+        Optional<Pruefung> pruefung = pruefungService.getPruefung(id);
+
+        if (pruefung.isPresent()) {
+            return pruefung.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
 
     @GetMapping("")
     public List<Pruefung> getAll(HttpServletResponse response) {

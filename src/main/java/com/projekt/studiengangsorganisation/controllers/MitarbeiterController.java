34ed3eb60ff0e675e286cff86b271c1f4e830618
Finn Plassmeier <finn@plassmeier.email>
2024-05-09 12:53:34 +0200

    PathVariable

--- src/main/java/com/projekt/studiengangsorganisation/controllers/MitarbeiterController.java
+++ src/main/java/com/projekt/studiengangsorganisation/controllers/MitarbeiterController.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,7 +24,7 @@ public class MitarbeiterController {
     MitarbeiterService mitarbeiterService;
 
     @GetMapping("/{id}")
-    public Mitarbeiter getOne(@RequestParam String id) {
+    public Mitarbeiter getOne(@PathVariable String id) {
         Optional<Mitarbeiter> mitarbeiter = mitarbeiterService.getMitarbeiter(id);
 
         if (mitarbeiter.isPresent()) {

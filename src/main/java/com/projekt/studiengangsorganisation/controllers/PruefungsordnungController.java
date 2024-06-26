a6ff8cebe5f4afb843c0570dde2ec6986eecea4c
Finn Plassmeier <finn@plassmeier.email>
2024-06-26 21:14:51 +0200

    prchekc

--- src/main/java/com/projekt/studiengangsorganisation/controllers/PruefungsordnungController.java
+++ src/main/java/com/projekt/studiengangsorganisation/controllers/PruefungsordnungController.java
@@ -133,14 +133,14 @@ public class PruefungsordnungController {
                 .getStudiengang(pruefungsordnung.getStudiengangId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Studiengang nicht gefunden"));
 
+        // Den abgerufenen Studiengang der Prüfungsordnung zuweisen
+        pruefungsordnung.setStudiengang(studiengang);
+
         // Überprüfen, ob es bereits eine Prüfungsordnung mit dem gleichen Studiengang und
         // Version gibt
         if (pruefungsordnungService.getPruefungsordnung(pruefungsordnung.getVersion(), pruefungsordnung.getStudiengang()).isPresent()) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Pruefungsordnung bereits vorhanden");
         }
-
-        // Den abgerufenen Studiengang der Prüfungsordnung zuweisen
-        pruefungsordnung.setStudiengang(studiengang);
 
         // Die aktualisierte Prüfungsordnung speichern und HTTP-Statuscode 201 (Created)
         // zurückgeben
